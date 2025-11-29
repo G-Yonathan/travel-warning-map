ca8f7dc23a618b9157968bc39e6ece453fd14abe
Jonathan <Jonathan@gmail.com>
2025-11-29 12:38:59 +0200

    updated about page

--- travel-map/src/About/About.jsx
+++ travel-map/src/About/About.jsx
@@ -6,10 +6,14 @@ export default function MapChart() {
             <div className="about-content">
                 <h2>About Azharot<span className="highlight">Masah</span></h2>
                 <p>
-                    ...
+                    This application provides a visual map interface to view Israeli government travel warnings.
                 </p>
                 <p>
-                    <strong>Disclaimer:</strong> ...
+                    <strong>Disclaimer:</strong> This tool is <strong>not</strong> an official source for government travel warnings. Rely exclusively on the <a href="https://www.gov.il/en/departments/dynamiccollectors/travel-warnings-nsc?skip=0" target="blank" rel="noopener noreferrer">official source</a> before making travel plans.
+                </p>
+                <hr className="soft-hr" />
+                <p>
+                    Created by <a href="https://github.com/G-Yonathan" target="_blank" rel="noopener noreferrer">G-Yonathan</a>
                 </p>
             </div>
         </div>
